9e02c8558c160cc0e8842f8b13f502a4444d444c
Tobias Hettler <Tobias-Hettler@protonmail.com>
2020-12-22 12:49:26 +0100

    Update java solution for day 03

--- 2020/03/Main.java
+++ 2020/03/Main.java
@@ -1,10 +1,7 @@
-package de.txbias;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigInteger;
 
 public class Main {
 
@@ -20,38 +17,34 @@ public class Main {
 
         String[] lines = content.split("\n");
 
-        BigInteger rightOneDownOne = BigInteger.valueOf(traverseMap(1, 1, lines));
-        System.out.println("Right 1, down 1: " + rightOneDownOne);
+        long rightOneDownOne = traverseMap(1, 1, lines);
 
-        BigInteger rightThreeDownOne = BigInteger.valueOf(traverseMap(3, 1, lines));
-        System.out.println("Right 3, down 1 (Part 1): " + rightThreeDownOne);
+        long rightThreeDownOne = traverseMap(3, 1, lines);
+        System.out.println("Part 1: " + rightThreeDownOne);
 
-        BigInteger rightFiveDownOne = BigInteger.valueOf(traverseMap(5, 1, lines));
-        System.out.println("Right 5, down 1: " + rightFiveDownOne);
+        long rightFiveDownOne = traverseMap(5, 1, lines);
 
-        BigInteger rightSevenDownOne = BigInteger.valueOf(traverseMap(7, 1, lines));
-        System.out.println("Right 7, down 1: " + rightSevenDownOne);
+        long rightSevenDownOne = traverseMap(7, 1, lines);
 
-        BigInteger rightOneDownTwo = BigInteger.valueOf(traverseMap(1, 2, lines));
-        System.out.println("Right 1, down 2: " + rightOneDownTwo);
+        long rightOneDownTwo = traverseMap(1, 2, lines);
 
         // Multiply all results together
-        BigInteger product = BigInteger.ONE;
-        product = product.multiply(rightOneDownOne);
-        product = product.multiply(rightThreeDownOne);
-        product = product.multiply(rightFiveDownOne);
-        product = product.multiply(rightSevenDownOne);
-        product = product.multiply(rightOneDownTwo);
+        long product = 1;
+        product *= rightOneDownOne;
+        product *= rightThreeDownOne;
+        product *= rightFiveDownOne;
+        product *= rightSevenDownOne;
+        product *= rightOneDownTwo;
 
         System.out.println("Part 2: " + product);
     }
 
-    private static int traverseMap(int right, int down, String[] map) {
+    private static long traverseMap(int right, int down, String[] map) {
         // The position coordinates
         int x = 0;
         int y = 0;
 
-        int encounteredTrees = 0;
+        long encounteredTrees = 0;
 
         while (y < map.length) {
             if (x > map[0].length() - 1) {
@@ -89,3 +82,4 @@ public class Main {
     }
 
 }
+
